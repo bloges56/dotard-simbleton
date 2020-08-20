43f95518d835016df5004a904c8c9fd979a2376f
Brady Logan <brady.drew.logan@gmail.com>
2020-08-20 10:12:10 -0500

    filterd for only new york businesses and put city, state, and zip in its own div

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -2,11 +2,13 @@ import { Business } from './Business.js'
 import {  useBusinesses } from './BusinessProvider.js'
 
 export const BusinessList = () => {
-    const contentElement = document.getElementById("business_list");
-    const businesses = useBusinesses();
+    const contentElement = document.getElementById("business_list_new_york");
+    const businesses = useBusinesses;
+
+    console.log(businesses);
 
     var businessHTML = `
-        <h2>Active Businesses</h2>
+        <h2>New York Businesses</h2>
     `;
 
     businesses.forEach((business) => {
